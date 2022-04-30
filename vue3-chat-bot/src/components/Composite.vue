b82dcc82df41fce43f6b39f67fb383b8335ae62f
dirtyrabbit <s8888863@gmail.com>
2022-04-30 23:57:47 +0800

    bulid botchat header

---
+++ vue3-chat-bot/src/components/Composite.vue
@@ -0,0 +1,25 @@
+<script setup>
+import ChatHeader from './ChatHeader.vue'
+import { UserFilled } from '@element-plus/icons-vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+    <div style="display: flex; align-items: center">
+
+    <el-popover
+      placement="top-start"
+      :width="300"
+      trigger="click"
+      popper-style="box-shadow: rgb(14 18 22 / 35%) 0px 10px 38px -10px, rgb(14 18 22 / 20%) 0px 10px 20px -15px; padding: 20px;"
+    >
+      <template #reference>
+        <el-avatar :icon="UserFilled" />
+      </template>
+
+      <template #default>
+        <chat-header></chat-header>
+      </template>
+    </el-popover>
+  </div>
+</template>
