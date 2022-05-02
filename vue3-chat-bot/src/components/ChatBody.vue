fea732e2303be0ed9bc49f64ed9e46b8140f9566
dirtyrabbit <s8888863@gmail.com>
2022-05-02 21:04:15 +0800

    Revise the message's colume, modify scrollbar for auto-down

--- vue3-chat-bot/src/components/ChatBody.vue
+++ vue3-chat-bot/src/components/ChatBody.vue
@@ -3,32 +3,51 @@
     <!-- chat display -->
     <el-row>
       <el-col :span="24">
-        <el-scrollbar height="300px">
+        <el-scrollbar ref="scrollbarRef" height="250px">
+          <div v-for="value in msgList" :key="value">
+            <el-row v-if="value.botmsg != null">
+              <div
+                style="
+                  display: flex;
+                  align-items: center;
+                  justify-content: left;
+                  margin: 5px;
+                "
+              >
+                <!-- Print bot response -->
+                <el-avatar :size="25" :icon="UserFilled" />
+              </div>
+              <div>
+                <div class="msgbox">
+                  <div class="msgcontent">
+                    {{ value.botmsg }}
+                  </div>
+                </div>
+              </div>
+            </el-row>
 
-          <div v-for="(value ) in msgList" :key="value">
-            <div class = "botmsg" align="left">
-              {{ value.botmsg }}
-            </div>
-            <div class = "botmsg" align="right">
-              {{ value.input }}
-            </div>
+            <!-- Print user input -->
+            <el-row v-if="value.input != null" justify="end">
+              <div class="msgbox">
+                <div class="msgcontent">{{ value.input }}</div>
+              </div>
+            </el-row>
           </div>
-
-          <hot-button></hot-button>
         </el-scrollbar>
       </el-col>
     </el-row>
+    <el-row style="height: 48px">
+      <hot-button></hot-button>
+    </el-row>
 
     <!-- chat input -->
-    <el-row>
-      <div>
-        <el-col :span="20">
-          <el-input v-model="input" placeholder="Please input" />
-        </el-col>
-        <el-col :span="2">
-          <el-button type="primary" @click="sentMsg" :icon="Promotion" />
-        </el-col>
-      </div>
+    <el-row class="inputrow" justify="center">
+      <el-col :span="19">
+        <el-input v-model="input" placeholder="Please input" />
+      </el-col>
+      <el-col :span="4">
+        <el-button type="primary" @click="sentMsg" :icon="Promotion" />
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -36,7 +55,14 @@
 <script setup lang="ts">
 import HotButton from "./HotButton.vue";
 import { Promotion } from "@element-plus/icons-vue";
-import { ref } from "vue";
+import { ref, nextTick } from "vue";
+import { UserFilled } from "@element-plus/icons-vue";
+
+const value = ref(0);
+import type { ElScrollbar } from "element-plus";
+const innerRef = ref<HTMLDivElement>();
+const scrollbarRef = ref<InstanceType<typeof ElScrollbar>>();
+
 const input = ref("");
 const botmsg = ref("Hi, I'm helper bot, May I help you?");
 const msgList = ref([{}]);
@@ -45,7 +71,7 @@ msgList.value.push({ botmsg: botmsg.value });
 //Regular Expression
 function isValid(str) {
   return /^\w+$/.test(str);
-} 
+}
 
 function sentMsg() {
   if (isValid(input.value)) {
@@ -53,10 +79,17 @@ function sentMsg() {
     returnMsg();
   }
   input.value = "";
+  scrollDown();
 }
 
 function returnMsg() {
   msgList.value.push({ botmsg: "I not ready" });
+}
+
+function scrollDown() {
+  nextTick(() => {
+    scrollbarRef.value!.setScrollTop(scrollbarRef.value.wrap$.scrollHeight);
+  });
 }
 </script>
 
@@ -64,6 +97,25 @@ function returnMsg() {
 .content {
   height: 300px;
   background-color: #dedfe0;
+  /* margin: 5px; */
+}
+.msgbox {
+  display: flex;
+  justify-content: center;
+  margin: 5px;
+  align-items: center;
+  min-height: 24px;
+  max-width: 140px;
+  background-color: #fff;
+  border-radius: 10px;
+}
+.msgcontent {
+  display: flex;
+  margin: 5px;
+  min-width: 36px;
+  justify-content: center;
+}
+.inputrow {
   margin: 5px;
 }
 .scrollbar-demo-item {
@@ -76,5 +128,12 @@ function returnMsg() {
   border-radius: 4px;
   background: var(--el-color-primary-light-9);
   color: var(--el-color-primary);
+}
+.el-row {
+  margin-top: 5px;
+}
+.el-col {
+  margin-right: 5px;
+  border-radius: 4px;
 }
 </style>
