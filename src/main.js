ff2a09778703a4f838a38229ce454652e81600d1
dirtyrabbit <s8888863@gmail.com>
2022-05-03 18:22:45 +0800

    Change module.exports

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-const app = createApp(App)
+const Vue3ChatBot = createApp(App)
 
-app.use(ElementPlus)
-app.mount('#app')
+Vue3ChatBot.use(ElementPlus)
+Vue3ChatBot.mount('#app')
 
-module.exports = app;
+module.exports = Vue3ChatBot;
